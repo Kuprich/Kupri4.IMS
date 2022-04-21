9116ca241ebe7c5c4383eb26e5b7ed41b684c7ea
Kupri4 <ilyakyp13rus@gmail.com>
2022-04-21 22:46:17 +0300

    show ErrorMessage when pruducing product

---
+++ Kupri4.IMS.WebApp/wwwroot/Main.js
@@ -0,0 +1,50 @@
+﻿
+function fadeOut(el) {
+
+	var opacity = 1;
+
+	var timer = setInterval(function () {
+
+		if (opacity <= 0.1) {
+
+			clearInterval(timer);
+			document.querySelector(el).style.display = "none";
+
+		}
+
+		document.querySelector(el).style.opacity = opacity;
+
+		opacity -= opacity * 0.1;
+
+	}, 5);
+}
+
+function fadeOutWithDelay(el, ms) {
+	setTimeout(() => fadeOut(el), ms);
+}
+
+
+function fadeIn(el) {
+
+	var opacity = 0.01;
+
+	document.querySelector(el).style.display = "block";
+
+	var timer = setInterval(function () {
+
+		if (opacity >= 1) {
+
+			clearInterval(timer);
+
+		}
+
+		document.querySelector(el).style.opacity = opacity;
+
+		opacity += opacity * 0.7;
+
+	}, 5);
+
+}
+
+
+
